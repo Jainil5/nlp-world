780616f44759ce7a44ceae4890b30f05307621f6
effyAI <jainiljp72525@gmail.com>
2023-12-09 20:15:51 +0530

    Internal changes

--- src/main.py
+++ src/main.py
@@ -5,25 +5,26 @@ from summarization_bart import get_summary
 # https://drive.google.com/uc?id=1qEFRPuwAJxxjORe-6vaC-WyLgTot_vaF
 st.title("Welcome to NLP WORLD")
 
-tab1, tab2 = st.tabs(["SENTIMENT ANALYSIS", "TEXT SUMMARIZATION"])
+tab1, tab2, tab3 = st.tabs(["HOME PAGE","SENTIMENT ANALYSIS", "TEXT SUMMARIZATION"])
 
 with tab1:
-    st.header("TEXT INPUT FOR SENTIMENT ANALYSIS")
-    if prompt := st.text_input(label=" ",placeholder=' Enter your text here to get the sentiment ...', key="a"):
+    st.header("NLP RELATED TASK DEVELOPMENT")
+
+with tab2:
+    if prompt := st.text_input(label="TEXT INPUT FOR SENTIMENT ANALYSIS",placeholder=' Enter your text here to get the sentiment ...', key="a"):
             response = get_sentiment(prompt)
             st.header("Sentiment Analysis Result:")
             st.header(str(response).upper())
             st.write(" ")
-            st.write("Model used for summarization: " + "RoBERTa")
+            # st.write("Model used for summarization: " + "RoBERTa")
 
-with tab2:
-    st.header("TEXT INPUT FOR SUMMARIZATION")
-    if prompt := st.text_input(label=" ",placeholder=' Enter your text here to get the summary ...', key = "b"):
+with tab3:
+    if prompt := st.text_input(label="TEXT INPUT FOR SUMMARIZATION",placeholder=' Enter your text here to get the summary ...', key = "b"):
             response = get_summary(prompt)
             st.header("Summary Result: ")
             st.write(response)
             st.write(" ")
-            st.write("Model used for summarization: " + "BART")
+            # st.write("Model used for summarization: " + "BART")
 
 
 
